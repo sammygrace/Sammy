28863044a884c1d202200e4dfa4cc12e6dd1f6a9
Sammy <sammy@lee-family>
2019-06-21 19:09:11 -0700

    got rid of unwanted stuff added faker got rid of phone, email, and website on home page made a search box got rid of "show"

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -4,7 +4,7 @@ class VendorsController < ApplicationController
   # GET /vendors
   # GET /vendors.json
   def index
-    @vendors = Vendor.all
+    @vendors = Vendor.where("content ilike ? or address ilike ?", "%#{params[:find]}%", "%#{params[:find]}%")
   end
 
   # GET /vendors/1
